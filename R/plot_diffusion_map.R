56c7be1c1122e3eb8333be03ce6dceb1c86b239b
yun <yunyunpp96@outlook.com>
2023-02-12 18:52:04 +0800

    fix wrong colour_by

--- R/plot_diffusion_map.R
+++ R/plot_diffusion_map.R
@@ -24,7 +24,7 @@
 #'   arguments passed to [geom_path][ggplot2::geom_path]; and for
 #'   `plot_dpt_paths`, other arguments passed to [add_dpt_paths]
 #' @name plot_diffusion_map
-#' @return 
+#' @return
 #'   - `plot_diffusion_map`: A ggplot2 object depicting the Diffusion map, if
 #'     add_paths is `TRUE`, DPT of the root is also added in this map.
 #'   - `plot_dpt_paths`: A ggplot2 object depicting the DPT paths.
@@ -40,7 +40,7 @@ plot_diffusion_map <- function(x, colour_by = "DPT", add_paths = TRUE, root = NU
     if (!rlang::is_scalar_logical(add_paths)) {
         cli::cli_abort("{.arg add_paths} must be a scalar logical value.")
     }
-    if (identical(x, "DPT") || add_paths) {
+    if (identical(colour_by, "DPT") || add_paths) {
         assert_length(root, 1L, null_ok = TRUE)
         assert_class(root, is.numeric, "numeric", null_ok = TRUE)
         branch_data <- x$destiny_branch
@@ -54,7 +54,7 @@ plot_diffusion_map <- function(x, colour_by = "DPT", add_paths = TRUE, root = NU
             root <- as.integer(root)
         }
         dpt_data <- dpt_data[[root]]
-        if (identical(x, "DPT")) {
+        if (identical(colour_by, "DPT")) {
             colour_by <- S4Vectors::DataFrame(DPT = dpt_data)
         }
     }
@@ -124,9 +124,10 @@ add_dpt_paths <- function(x, root = NULL, paths_to = NULL, w_width = 0.1, ..., n
 }
 
 #' @rdname plot_diffusion_map
-#' @export 
+#' @export
 plot_dpt_paths <- function(...) {
-    ggplot2::ggplot() + add_dpt_paths(...)
+    ggplot2::ggplot() +
+        add_dpt_paths(...)
 }
 
 geom_dpt_paths_helper <- function(root = integer(), paths_to, branch, dpt, evs, w_width = 0.1, ...) {
