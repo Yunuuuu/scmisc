20f5dc57e2c588c7c61ad0bc17b86ad17607a30a
yun <yunyunpp96@outlook.com>
2023-02-13 12:48:01 +0800

    enable the path_args deliveried to geom_path separetely

--- R/plot_diffusion_map.R
+++ R/plot_diffusion_map.R
@@ -2,7 +2,7 @@
 #' @param x A [SingleCellExperiment][SingleCellExperiment::SingleCellExperiment]
 #'   object.
 #' @param y A [DiffusionMap][destiny::DiffusionMap] or [DPT][destiny::DPT]
-#'   object. 
+#'   object.
 #' @param ... Other arguments passed to
 #'   [plotReducedDim][scater::plotReducedDim].
 #' @name plot_diffusion_map
@@ -39,14 +39,18 @@ setMethod(
 #'   result in `reducedDims(x)` to plot.
 #' @param colour_by Specification of a column metadata field or a feature to
 #'   colour by, see the by argument in
-#'   [`?retrieveCellInfo`][scater::retrieveCellInfo()] for possible values.
+#'   [`?retrieveCellInfo`][scater::retrieveCellInfo()] for possible values. IF y
+#'   is a [DPT][destiny::DPT] object, this can also be "DPT" (or starting by
+#'   "DPT", such as "DPT1", "DPT2" ...), "branch" or "Branch" e.g. which will be
+#'   extracted from `y`. See also [plot.DPT][destiny::plot.DPT].
 #' @param add_paths A scalar logical value indicates whether to add path.
 #' @param root Root branch ID. Will be used as the start of the DPT. (default:
 #'   lowest branch ID).
-#' @param paths_to Numeric Branch IDs. Are used as target(s) for the path(s) to
-#'   draw.
+#' @param paths_to Numeric Branch IDs, which are used as target(s) for the
+#'   path(s) to draw.
 #' @param path_args A list of arguments to passed to
-#'   [geom_path][ggplot2::geom_path].
+#'   [geom_path][ggplot2::geom_path]. Elements of the list can be the same
+#'   length of `paths_to`.
 #' @param w_width Window width for smoothing the path (see
 #'   [smth.gaussian][smoother::smth.gaussian]).
 #' @param ncomponents A numeric scalar indicating the number of dimensions to
@@ -60,11 +64,17 @@ setMethod(
     c("SingleCellExperiment", "DPT"),
     function(x, y, dimred = "DiffusionMap", colour_by = "DPT",
              add_paths = TRUE, root = NULL, paths_to = NULL,
-             path_args = list(), w_width = 0.1, ..., ncomponents = 2L) {
+             path_args = list(color = rev(palette())), 
+             w_width = 0.1, ..., ncomponents = 2L) {
         if (!rlang::is_scalar_logical(add_paths)) {
             cli::cli_abort("{.arg add_paths} must be a scalar logical value.")
         }
-        if (identical(colour_by, "DPT") || add_paths) {
+
+        is_colour_by_destiny_data <- grepl("^DPT", colour_by, perl = TRUE) ||
+            identical(colour_by, "Branch") ||
+            identical(colour_by, "branch")
+
+        if (is_colour_by_destiny_data || add_paths) {
             assert_length(root, 1L, null_ok = TRUE)
             assert_class(root, is.numeric, "numeric", null_ok = TRUE)
 
@@ -82,8 +92,18 @@ setMethod(
             }
             dpt_data <- dpt_data[[root]]
         }
-        if (identical(colour_by, "DPT")) {
-            colour_by <- S4Vectors::DataFrame(DPT = dpt_data)
+
+        if (is_colour_by_destiny_data) {
+            colour_by <- rlang::exec(
+                quote(S4Vectors::DataFrame),
+                !!colour_by := switch(colour_by,
+                    DPT = dpt_data,
+                    branch = ,
+                    Branch = branch_data,
+                    y[[colour_by]]
+                ),
+                check.names = FALSE
+            )
         }
         if (add_paths || identical(dimred, "DiffusionMap")) {
             dm <- y@dm
@@ -114,32 +134,39 @@ setMethod(
                 ncomponents <- seq_len(ncomponents)
             }
             evs <- destiny::eigenvectors(dm)[, ncomponents, drop = FALSE]
-            plot_res <- plot_res + rlang::inject(
-                geom_dpt_paths_helper(
-                    root = root, paths_to = paths_to,
-                    branch = branch_data,
-                    dpt = dpt_data,
-                    evs = evs,
-                    w_width = w_width,
-                    !!!path_args
-                )
+            plot_res <- plot_res + geom_dpt_paths_helper(
+                root = root, paths_to = paths_to,
+                branch = branch_data,
+                dpt = dpt_data, evs = evs,
+                w_width = w_width,
+                path_args = path_args
             )
         }
         plot_res
     }
 )
 
-geom_dpt_paths_helper <- function(root = integer(), paths_to, branch, dpt, evs, w_width = 0.1, ...) {
+geom_dpt_paths_helper <- function(root = integer(), paths_to, branch, dpt, evs, w_width = 0.1, path_args = list()) {
     if (is.null(paths_to)) {
         paths_to <- setdiff(branch[!is.na(branch)], root)
     }
+    if (length(paths_to) > 0L) {
+        args_list <- list(path_id = paths_to)
+    } else {
+        return(NULL)
+    }
     evs <- as.data.frame(evs)
     if (ncol(evs) >= 2L) {
-        names(evs)[1:2] <- c("x", "y")
+        evs <- evs[1:2]
+        names(evs) <- c("x", "y")
     } else {
         cli::cli_abort("Cannot plot with less than 2 {.arg ncomponents}")
     }
-    paths <- lapply(paths_to, function(path_id) {
+    if (length(path_args) > 0L) {
+        path_args <- lapply(path_args, rep_len, length.out = length(paths_to))
+        args_list <- c(args_list, path_args)
+    }
+    .mapply(function(path_id, ...) {
         path_idx <- which(branch %in% c(root, path_id))
         path_pt <- dpt[path_idx]
         path_evs <- evs[path_idx, , drop = FALSE]
@@ -154,6 +181,5 @@ geom_dpt_paths_helper <- function(root = integer(), paths_to, branch, dpt, evs, 
             inherit.aes = FALSE,
             ...
         )
-    })
-    paths
+    }, args_list, MoreArgs = NULL)
 }
