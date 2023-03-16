b5d57941cceb9e483b8d899e94971f1f9370bb0d
yun <yunyunpp96@outlook.com>
2023-03-16 16:19:54 +0800

    add argument circle_gp

--- R/DotsHeatmap.R
+++ R/DotsHeatmap.R
@@ -4,6 +4,8 @@
 #'  color.
 #' @param matrix_size A numeric matrix or vector. If it is a simple vector, it
 #'  will be converted to a one-column matrix. Only used for dot size.
+#' @param circle_gp Graphic parameters for drawing point circle (for heatmap
+#'   body). The value should be specified by gpar and fill parameter is ignored.
 #' @param radius_range A numeric vector of length one or two specifying the
 #'  minimal and maximal dots radius (measured in "mm"). If length one, all dots
 #'  will have the same size. If length two with different values, the
@@ -28,7 +30,7 @@
 #'   [Heatmap][ComplexHeatmap::Heatmap] object.
 #' @name DotsHeatmap
 #' @export
-DotsHeatmap <- function(matrix, matrix_size = NULL, radius_range = c(1, 5), dots_size_legend_param = list(), slice_border_gp = NULL, ...) {
+DotsHeatmap <- function(matrix, matrix_size = NULL, circle_gp = gpar(col = NA), radius_range = c(1, 5), dots_size_legend_param = list(), slice_border_gp = NULL, ...) {
     assert_class(radius_range, "numeric", is.numeric)
     if (!data.table::between(length(radius_range), 1L, 2L)) {
         cli::cli_abort("{.arg radius_range} must be a numeric with a length 1 or 2")
@@ -41,6 +43,14 @@ DotsHeatmap <- function(matrix, matrix_size = NULL, radius_range = c(1, 5), dots
     matrix_size <- matrix_size %||% matrix
     assert_class(matrix_size, "numeric", is.numeric)
     if (!is.matrix(matrix)) {
+        if (is.atomic(matrix)) {
+            cli::cli_alert_info("convert simple vector to one-column matrix")
+            matrix <- matrix(matrix, ncol = 1L)
+        } else {
+            cli::cli_abort("{.arg matrix} must be a matrix or a simple vector.")
+        }
+    }
+    if (!is.matrix(matrix_size)) {
         if (is.atomic(matrix_size)) {
             cli::cli_alert_info("convert simple vector to one-column matrix")
             matrix_size <- matrix(matrix_size, ncol = 1L)
@@ -56,14 +66,15 @@ DotsHeatmap <- function(matrix, matrix_size = NULL, radius_range = c(1, 5), dots
         slice_border_gp$fill <- "transparent"
     }
     heatmap <- ComplexHeatmap::Heatmap(
-        matrix,
+        matrix = matrix,
         rect_gp = gpar(type = "none"),
         layer_fun = function(j, i, x, y, width, height, fill) {
             size_values <- ComplexHeatmap::pindex(matrix_size, i = i, j = j)
+            circle_gp$fill <- fill
             grid::grid.circle(
                 x = x, y = y,
                 r = scale_size(size_values),
-                gp = gpar(fill = fill, col = NA)
+                gp = circle_gp
             )
             if (!is.null(slice_border_gp)) {
                 grid::grid.rect(gp = slice_border_gp)
@@ -73,55 +84,56 @@ DotsHeatmap <- function(matrix, matrix_size = NULL, radius_range = c(1, 5), dots
     )
     if (is.null(dots_size_legend_param)) {
         return(heatmap)
+    } else {
+        dots_size_legend_param <- do.call(
+            prepare_legend_param_for_dots_size,
+            c(dots_size_legend_param, list(
+                matrix = matrix_size, scale_size = scale_size
+            ))
+        )
     }
 
-    dots_size_legend_param$at <- unique(dots_size_legend_param$at) %||%
-        scales::breaks_extended()(matrix_size)
-    if (!is.null(dots_size_legend_param$radius_range)) {
-        dots_size_legend_param$radius <- scale_range(
-            unclass(dots_size_legend_param$radius_range), matrix_size
-        )(dots_size_legend_param$at)
-        dots_size_legend_param$radius_range <- NULL
-    } else if (!is.null(dots_size_legend_param$radius)) {
-        breaks_len <- length(dots_size_legend_param$at)
-        radius_len <- length(dots_size_legend_param$radius)
-        if (radius_len != breaks_len) {
-            cli::cli_abort(c(
-                "{.arg dots_size_legend_param$radius} and {.arg dots_size_legend_param$at} must have same length",
-                i = "the length of {.arg at}: {.val {breaks_len}}",
-                i = "the length of {.arg radius}: {.val {radius_len}}"
-            ))
-        }
-        dots_size_legend_param$radius <- grid::unit(
-            dots_size_legend_param$radius, "mm"
-        )
-    } else {
-        dots_size_legend_param$radius <- scale_size(dots_size_legend_param$at)
-    }
-    dots_size_legend_param$graphics <- vector("list",
-        length = length(dots_size_legend_param$radius)
-    )
-    # lapply will omit unit attributes, so we just use for loop
-    for (i in seq_along(dots_size_legend_param$graphics)) {
-        dots_size_legend_param$graphics[[i]] <- circle_legend(
-            dots_size_legend_param$radius[[i]]
-        )
-    }
-    dots_size_legend_param$radius <- NULL
     methods::new("DotsHeatmap",
         heatmap = heatmap,
         dots_legend = do.call(ComplexHeatmap::Legend, dots_size_legend_param)
     )
 }
 
+prepare_legend_param_for_dots_size <- function(matrix, at = NULL, radius_range = NULL, radius = NULL, ..., scale_size) {
+    at <- unique(at) %||% scales::breaks_extended()(matrix)
+    if (!is.null(radius_range)) {
+        radius <- scale_range(unclass(radius_range), matrix)(at)
+    } else if (!is.null(radius)) {
+        breaks_len <- length(at)
+        radius_len <- length(radius)
+        if (radius_len != breaks_len) {
+            cli::cli_abort(c(
+                "{.arg radius} and {.arg at} must have the same length",
+                i = "the length of {.arg at}: {.val {breaks_len}}",
+                i = "the length of {.arg radius}: {.val {radius_len}}"
+            ))
+        }
+        # unit function will not omit the existed units
+        radius <- grid::unit(radius, "mm")
+    } else {
+        radius <- scale_size(at)
+    }
+    graphics <- vector("list", length = length(radius))
+    # lapply will omit unit attributes, so we just use for loop
+    for (i in seq_along(graphics)) {
+        graphics[[i]] <- circle_legend(radius[[i]])
+    }
+    list(at = at, graphics = graphics)
+}
+
 #' @return A scale function taking a numeric vector and return a scaled unit
 #'   values.
 #' @noRd
-scale_range <- function(radius_range, matrix) {
-    force(radius_range)
+scale_range <- function(range, matrix) {
+    force(range)
     from <- range(matrix, na.rm = TRUE, finite = TRUE)
     function(x) {
-        x <- scales::rescale(x, to = radius_range, from = from)
+        x <- scales::rescale(x, to = range, from = from)
         grid::unit(x, "mm")
     }
 }
