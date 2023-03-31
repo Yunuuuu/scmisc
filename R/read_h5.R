f101bc237bcedcfc5235756289a0017070834584
yun <yunyunpp96@outlook.com>
2023-03-31 23:17:27 +0800

    fix R CMD check warnings

--- R/read_h5.R
+++ R/read_h5.R
@@ -1,5 +1,11 @@
 #' Reads object in HDF5 files
-#' @inheritParams rhdf5::h5read
+#' @param file The filename (character) of the file in which the dataset is be
+#'  located. It is possible to provide an object of class
+#'  [H5IdComponent][rhdf5::H5IdComponent-class] representing a H5 location
+#'  identifier (file or group). See [H5Fcreate][rhdf5::H5Fcreate],
+#'  [H5Fopen][rhdf5::H5Fopen], [H5Gcreate][rhdf5::H5Gcreate],
+#'  [H5Gopen][rhdf5::H5Gopen] to create an object of this kind. 
+#' @param name The name of the dataset in the HDF5 file. 
 #' @inheritDotParams rhdf5::h5read -file -name
 #' @return A sparse matrix
 #' @export 
